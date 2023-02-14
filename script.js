005de7fd8cd7b0275cd6efb4a1e3d353967690eb
Victor <victorgaspalmeida@hotmail.com>
2023-02-14 15:06:57 -0300

    accordion list

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const tabMenu = document.querySelectorAll('.js-tabMenu li');
-const tabContent = document.querySelectorAll('.js-tabContent section');
-
-if(tabMenu.length && tabContent.length){
-    tabContent[0].classList.add('ativo'); /* deixar primeiro elemento ativo como padr]ao */
-
-    function activeTab(index){
-        tabContent.forEach((item) => {
-            item.classList.remove('ativo');
-        })
-        tabContent[index].classList.add('ativo');
-    }
+function initTabNav(){
+    const tabMenu = document.querySelectorAll('.js-tabMenu li');
+    const tabContent = document.querySelectorAll('.js-tabContent section');
     
-    tabMenu.forEach((item, index) =>{
-        item.addEventListener('click', () =>{
-            activeTab(index)
+    if(tabMenu.length && tabContent.length){
+        tabContent[0].classList.add('ativo'); /* deixar primeiro elemento ativo como padr]ao */
+    
+        function activeTab(index){
+            tabContent.forEach((item) => {
+                item.classList.remove('ativo');
+            })
+            tabContent[index].classList.add('ativo');
+        }
+        
+        tabMenu.forEach((item, index) =>{
+            item.addEventListener('click', () =>{
+                activeTab(index)
+            })
         })
-    })
+    }
 }
+
+function initAccordion(){
+    const ativo = 'ativo';
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    if(accordionList.length){
+        accordionList[0].classList.add(ativo);
+        accordionList[0].nextElementSibling.classList.add(ativo);
+        
+        function activeAccordion(){
+            this.classList.toggle(ativo);
+            this.nextElementSibling.classList.toggle(ativo);
+        }
+        
+        accordionList.forEach((item) =>{
+            item.addEventListener('click', activeAccordion);
+        });
+    }  
+}
+
+
+initTabNav();
+initAccordion();
