5bea1e78825d887a1aac10e93314ce71947817d6
Victor <victorgaspalmeida@hotmail.com>
2023-02-15 10:51:29 -0300

    animação no scroll

--- script.js
+++ script.js
@@ -55,6 +55,26 @@ function initScrollSuave(){
     })
 }
 
+function initAnimaScroll(){
+    const sections = document.querySelectorAll('.js-scroll');
+    if(sections.length){
+        const windowMetade = window.innerHeight * 0.6;
+    
+        function animaScroll(){
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top - windowMetade;
+                if(sectionTop < 0){
+                    section.classList.add('ativo');
+                }
+            })
+        }
+    
+        animaScroll();
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+
 initTabNav();
 initAccordion();
 initScrollSuave();
+initAnimaScroll();
