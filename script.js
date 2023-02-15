01c90efd5b776a52aa4096e1ea125a447b81a6fc
Victor <victorgaspalmeida@hotmail.com>
2023-02-15 10:22:05 -0300

    scroll suave para links internos

--- script.js
+++ script.js
@@ -31,13 +31,30 @@ function initAccordion(){
             this.classList.toggle(ativo);
             this.nextElementSibling.classList.toggle(ativo);
         }
-        
         accordionList.forEach((item) =>{
             item.addEventListener('click', activeAccordion);
         });
     }  
 }
 
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+    function scrollToSection(event){
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+    
+    linksInternos.forEach((link) =>{
+        link.addEventListener('click', scrollToSection);
+    })
+}
 
 initTabNav();
 initAccordion();
+initScrollSuave();
